7170de9dbc133aa26c580166095449c715f564ec
dannyut <dannyv40@gmail.com>
2015-12-11 22:50:55 -0800

    created nfl game scores project

---
+++ games.py
@@ -0,0 +1,14 @@
+# built using python 2.7.11
+# this took me 38.2s to finish
+import nflgame
+
+f = open('games_data','w')
+msg = 'week, away, home, score_away, score_home\n'
+#print(msg)
+f.write(msg)
+for i in range (1,15):
+	games = nflgame.games(2015, week=i)
+	for g in games:
+		msg = '%d, %s, %s, %d, :%d \n' % (i,g.away,g.home,g.score_away,g.score_home)
+		#print msg
+		f.write(msg)
